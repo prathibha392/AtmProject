8678c095338590136c774d73d7e383d33f477514
prathibha392 <prathibhasaik62@gmail.com>
2025-02-14 14:51:40 +0530

    ATM Project

---
+++ src/atm/AtmInterface.java
@@ -0,0 +1,11 @@
+package atm;
+
+public interface AtmInterface {
+  public void viewbalance();
+  public void withdrawAmount(double withdrawAmount);
+  public void depositAmount(double depositAmount);
+  public void viewMiniStatement();
+
+
+  
+}
